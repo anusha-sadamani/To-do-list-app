5292e8eea25e800917cc693523ab56a47de05a04
anusha-sadamani <anushams2k01@gmail.com>
2025-04-10 23:39:51 +0530

    save Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,39 @@
 const inputBox = document.getElementById("field");
 const task = document.getElementById("task-list");
 
-//funtion for createTaskElement
-
-function createTaskElement(taskValue) {
+// function to create task element
+function createTaskElement(taskValue, Checked = false) {
   let li = document.createElement("li");
   li.textContent = taskValue;
+  if (Checked === true) {
+    li.classList.add("checked");
+  }
   let span = document.createElement("span");
   span.textContent = "x";
   li.appendChild(span);
   return li;
 }
 
-//function for addTask
-
+// function to add task
 function addTask() {
   let taskValue = inputBox.value;
   if (taskValue === "") {
     alert("Please, Enter a Task!");
   }
+
   let existingTask = [...task.children].map((li) => li.firstChild.textContent);
   if (existingTask.includes(taskValue)) {
     alert("Task already exists!");
     inputBox.value = "";
   }
+
   let li = createTaskElement(taskValue);
   task.appendChild(li);
   inputBox.value = "";
   save();
 }
 
-// function for checking and removing
-
+// function to check or remove
 task.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
@@ -42,20 +44,29 @@ task.addEventListener("click", function (e) {
   }
 });
 
-// function for saving the task
-
+// function to save tasks
 function save() {
-  let tasks = [...task.children].map((li) => li.firstChild.textContent);
+  let tasks = [...task.children].map((li) => {
+    return {
+      text: li.firstChild.textContent,
+      checked: li.classList.contains("checked"),
+    };
+  });
+  //  let children = task.children;
+  // //   //  let task = [];
+  // //   //  for(i=0;i<=children.length;i++){
+  // //   //   task.push(children[i].firstChild.textContent);
+  // //   //  }
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// function for show items
-
+// function to load tasks
 function loadTasks() {
   let tasks = JSON.parse(localStorage.getItem("tasks"));
   tasks.forEach((taskValue) => {
-    let li = createTaskElement(taskValue);
+    let li = createTaskElement(taskValue.text, taskValue.checked);
     task.appendChild(li);
   });
 }
+
 loadTasks();
